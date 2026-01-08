18913dc7d07c70a19f84b3a22818ee7b01df6df5
SriLakshmiK1810 <srilakshmik1810@gmail.com>
2026-01-08 09:45:35 +0530

    Update lambda_function.py

--- backend/users/lambda_function.py
+++ backend/users/lambda_function.py
@@ -1,1 +1,15 @@
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            "message": "User Service is running",
+            "functions": [
+                "View Restaurants",
+                "View Menu",
+                "Place Order"
+            ]
+        })
+    }
 

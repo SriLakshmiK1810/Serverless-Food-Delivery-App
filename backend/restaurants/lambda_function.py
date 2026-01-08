7b926e2c44b7db1f179b4aec39346f01a8fa2f33
SriLakshmiK1810 <srilakshmik1810@gmail.com>
2026-01-08 09:41:04 +0530

    Update lambda_function.py

--- backend/restaurants/lambda_function.py
+++ backend/restaurants/lambda_function.py
@@ -1,1 +1,31 @@
 
+import json
+import boto3
+
+# Create DynamoDB resource
+dynamodb = boto3.resource('dynamodb')
+
+# Connect to Restaurants table
+table = dynamodb.Table('Restaurants')
+
+def lambda_handler(event, context):
+    try:
+        # Scan DynamoDB table
+        response = table.scan()
+        restaurants = response.get('Items', [])
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps(restaurants)
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "error": str(e)
+            })
+        }
